6545739c4fce49a6628d44ced9a10b253712dc09
Théo Cramez <theo.cramez@gmail.com>
2022-11-17 16:53:52 +0100

    add netdata + autoscaling policies

--- alb.tf
+++ alb.tf
@@ -21,7 +21,6 @@ resource "aws_security_group_rule" "allow_all_outbound" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.alb_sg.id
 }
-
 # Allow inbound HTTP traffic
 resource "aws_security_group_rule" "allow_http_public" {
   type              = "ingress"
@@ -36,6 +35,15 @@ resource "aws_security_group_rule" "allow_ssh_public" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id         = aws_security_group.alb_sg.id
+}
+# Allow inbound netdata traffic
+resource "aws_security_group_rule" "allow_netdata_public" {
+  type              = "ingress"
+  from_port         = 19999
+  to_port           = 19999
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id         = aws_security_group.alb_sg.id
@@ -64,6 +72,14 @@ resource "aws_lb_target_group" "alb_tg_http" {
   vpc_id   = "${module.discovery.vpc_id}"
 }
 
+# Create a target group for netdata
+resource "aws_lb_target_group" "alb_tg_netdata" {
+  name     = "${var.vpc_name}-netdata"
+  port     = 19999
+  protocol = "HTTP"
+  vpc_id   = "${module.discovery.vpc_id}"
+}
+
 # Create an ALB listener for HTTP -> HTTP target group
 resource "aws_lb_listener" "alb_listener_http" {
   load_balancer_arn = "${aws_lb.alb.arn}"
@@ -72,6 +88,18 @@ resource "aws_lb_listener" "alb_listener_http" {
 
   default_action {
     target_group_arn = "${aws_lb_target_group.alb_tg_http.arn}"
+    type             = "forward"
+  }
+}
+
+# Create an ALB listener for netdata target group
+resource "aws_lb_listener" "alb_listener_netdata" {
+  load_balancer_arn = "${aws_lb.alb.arn}"
+  port              = "19999"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.alb_tg_netdata.arn}"
     type             = "forward"
   }
 }
--- asg.tf
+++ asg.tf
@@ -29,6 +29,15 @@ resource "aws_security_group_rule" "allow_ssh_priv" {
   source_security_group_id = aws_security_group.alb_sg.id
 }
 
+resource "aws_security_group_rule" "allow_netdata_priv" {
+  type              = "ingress"
+  from_port         = 19999
+  to_port           = 19999
+  protocol          = "tcp"
+  security_group_id = aws_security_group.asg_sg.id
+  source_security_group_id = aws_security_group.alb_sg.id
+}
+
 resource "aws_security_group_rule" "allow_priv_outbound" {
   type              = "egress"
   from_port         = 0
@@ -63,4 +72,63 @@ resource "aws_autoscaling_group" "asg" {
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.asg.name
   alb_target_group_arn   = aws_lb_target_group.alb_tg_http.arn
+}
+
+resource "aws_autoscaling_attachment" "asg_netdata_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  alb_target_group_arn   = aws_lb_target_group.alb_tg_netdata.arn
+}
+
+# upwards autoscaling policies
+resource "aws_autoscaling_policy" "up_policy" {
+  name                   = "${var.vpc_name}-up-policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "up_alarm" {
+  alarm_name          = "Autoscaling-Alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "4"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.up_policy.arn]
+}
+
+# downwards autoscaling policies
+resource "aws_autoscaling_policy" "down_policy" {
+  name                   = "${var.vpc_name}-down-policy"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "down_alarm" {
+  alarm_name          = "Autoscaling-Alarm"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "4"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.down_policy.arn]
 }
